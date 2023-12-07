7c1478b377c647d1a1cee8f02f4717eda1f4d6b9
Akshaya15556 <140634152+Akshaya15556@users.noreply.github.com>
2023-12-06 21:51:45 -0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,7 +1,15 @@
-from .base import Base
-from .coin import Coin
-from .coin_value import CoinValue, Interval
-from .current_coin import CurrentCoin
-from .pair import Pair
-from .scout_history import ScoutHistory
-from .trade import Trade, TradeState
+import importlib
+import os
+
+
+def get_strategy(name):
+    for dirpath, _, filenames in os.walk(os.path.dirname(__file__)):
+        filename: str
+        for filename in filenames:
+            if filename.endswith("_strategy.py"):
+                if filename.replace("_strategy.py", "") == name:
+                    spec = importlib.util.spec_from_file_location(name, os.path.join(dirpath, filename))
+                    module = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(module)
+                    return module.Strategy
+    return None
